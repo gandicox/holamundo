bced5b8e76aa4cd04b632ae391f3ef5f51e67441
U. Mariano Gálvez <umg@labqtz.local>
2023-10-28 08:06:48 -0600

    hola

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+
+package holamundo;
+
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     System.out.print("Hola Mundo!");
+    }
+    
+}
